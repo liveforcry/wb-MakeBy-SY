1cbfc1d272bdbb77da365f432b0b04f817f8eff3
liveforcry <1013206350@qq.com>
2015-11-03 21:24:56 +0800

    1
    1

---
+++ shenyiWB/shenyiWB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shenyiWB
+//
+//  Created by apple-jd30 on 15/11/3.
+//  Copyright © 2015年 apple-jd30. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shenyiWB/shenyiWB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shenyiWB
+//
+//  Created by apple-jd30 on 15/11/3.
+//  Copyright © 2015年 apple-jd30. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
